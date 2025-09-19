3683394fee1dd623ee0124d796e8e42a34e07f6b
Brandon Tsai <87055878+Fire05s@users.noreply.github.com>
2025-09-18 19:45:42 -0700

    Fixed app.py & index.html to have proper form post processing

--- app.py
+++ app.py
@@ -13,20 +13,6 @@ def index():
         print(taskName, taskDate, taskDesc, taskParts)
     
     return render_template('index.html')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/submit_form', methods=['POST'])
-# def submit_form():
-#     if request.method == 'POST':
-#         taskName = request.form.get('addName')
-#         taskDate = request.form.get('addDate')
-#         taskDesc = request.form.get('addDesc')
-#         taskParts = request.form.get('addParts')
-
-#         print(taskName, taskDate, taskDesc, taskParts)
-
-#         return "Form submitted successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
